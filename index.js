dfb1c2ea5e8e2da7b4d0eea756a61a1e30352f8f
Pierce <pierceevans@me.com>
2022-12-27 16:53:23 -0600

    commit tree fe6daa1c5cdcce65ccd9f994727bfb054e2fc9cd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+]
+function superbowlWin(array){
+    let result = array.find(element => element.result === "W");
+    console.log(result);
+    if(result){
+        return result.year
+    }
+    else{
+        return result
+    }
+}
